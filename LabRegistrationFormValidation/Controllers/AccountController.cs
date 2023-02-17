ad0de6f993768b4484350570bac06d5d2e1ab009
Narendran2020 <72653314+Narendran2020@users.noreply.github.com>
2023-02-17 12:36:04 +0530

    InitialCommit

---
+++ LabRegistrationFormValidation/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using LabRegistrationFormValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabRegistrationFormValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserViewModel medel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ LabRegistrationFormValidation/Models/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using LabRegistrationFormValidation.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabRegistrationFormValidation.Models
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Email")]
+        [EmailAddress(ErrorMessage = "Please enter Correct EmailId")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please enter Password")]
+
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+         ErrorMessage = "Password required (1 Uppercase, 1 Number, 1 Special Char and 1 Lowercase Char) with Min Length 8 Chars")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Confirm Password doesn't match")]
+        [Required(ErrorMessage = "Please enter Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [RegularExpression(@"^[9187]\d{9}$", ErrorMessage = "Please enter correct Mobile Number")]
+        public string? Contact { get; set; }
+
+
+        [Required(ErrorMessage = "Gender is Required")]
+        public string Gender { get; set; }
+
+        [ValidateCheckBox(ErrorMessage ="Please accept Terms")]
+        public bool Terms { get; set; }
+    }
+}
